f0e016c5362feed257f8e67defe88083b189e171
Djonathan Assis Oliveira <djonathanassis@gmail.com>
2026-06-11 18:02:31 -0300

    docs(models): add PHPDoc property annotations for IDE autocompletion

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * @property int $id
+ * @property string $external_id
+ * @property string $name
+ * @property-read Wallet|null $wallet
+ * @property-read Collection<int, Transaction> $transactions
+ */
 class Player extends Model
 {
     use HasFactory;
--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property int $player_id
+ * @property string $balance
+ * @property string $currency
+ * @property-read Player|null $player
+ * @property-read Collection<int, Transaction> $transactions
+ */
 class Wallet extends Model
 {
     use HasFactory;
